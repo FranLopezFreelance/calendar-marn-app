b6febb98adc3865fe6729bc1a3e2a0006750e0fa
frLopez <frlopez@WKS928L>
2020-10-13 18:19:58 -0300

    add react modal

---
+++ src/components/calendar/CalendarModal.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    backgroundColor       : 'white'
+  }
+};
+
+Modal.setAppElement('#root');
+
+export const CalendarModal = () => {
+
+  const [isOpen, setIsOpen] = useState(true);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+      closeTimeoutMS={200}
+      className="modal"
+      overlayClassName="modal-fondo"
+    >
+      <h1>Hola Mundo</h1>
+      <hr />
+      <p>Modal</p>
+    </Modal>
+  )
+}
--- src/components/calendar/CalendarPage.js
+++ src/components/calendar/CalendarPage.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import { NavBar } from '../shared/NavBar';
 import { messages } from '../../helpers/calendarMessages';
 import moment from 'moment';
 import 'moment/locale/es';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 
 moment.locale('es');
@@ -14,7 +15,7 @@ const localizer = momentLocalizer(moment); // or globalizeLocalizer
 const events = [{
   title: 'Cumpleaños de Pedro',
   start: moment().subtract(4, 'hours').toDate(),
-  end: moment().subtract(2, 'hours').toDate(),
+  end: moment().subtract(1, 'hours').toDate(),
   notes: 'Comprar el wiskie',
   user: {
     _id: 102,
@@ -24,10 +25,24 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+  const onDoubleClick = (e) => {
+    console.log(e);
+  }
+
+  const onSelect = (e) => {
+    console.log(e);
+  }
+
+  const onView = (e) => {
+    setLastView(e);
+    localStorage.setItem('lastView', e);
+  }
+
   const eventPropGetter = (event, start, end, isSelected) => {
-    console.log(event, start, end, isSelected);
     const style = {
-      backgroundColor: '#59B086',
+      backgroundColor: '#002D8D',
       borderRadius: '0px',
       opacity: 0.8,
       display: 'block',
@@ -42,6 +57,7 @@ export const CalendarPage = () => {
     <div className="calendar-screen">
       <NavBar />
       <div className="container-fluid">
+        <CalendarModal />
         <Calendar
           localizer={localizer}
           events={events}
@@ -49,10 +65,15 @@ export const CalendarPage = () => {
           endAccessor="end"
           messages={messages}
           eventPropGetter={eventPropGetter}
+          onDoubleClickEvent={onDoubleClick}
+          onSelectEvent={onSelect}
+          onView={onView}
+          view={lastView}
           components={{
             event: CalendarEvent
           }}
         />
+        
       </div>
     </div>
   )
